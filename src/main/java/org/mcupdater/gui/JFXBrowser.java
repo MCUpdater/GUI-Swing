051547aafee565f0754f06d60d4a49c87183b639
Scott M. Barbour <smbarbour@gmail.com>
2014-12-16 20:51:05 -0600

    GUI-Swing 3.3.12 - JavaFX-based browser will open in new windows when appropriate

--- src/main/java/org/mcupdater/gui/JFXBrowser.java
+++ src/main/java/org/mcupdater/gui/JFXBrowser.java
@@ -1,14 +1,21 @@
 package org.mcupdater.gui;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.embed.swing.JFXPanel;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
+import javafx.scene.web.PopupFeatures;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
 
 public class JFXBrowser extends BrowserProxy {
 
@@ -32,6 +39,7 @@ public class JFXBrowser extends BrowserProxy {
 		WebView webView = new WebView();
 		group.add(webView);
 		webEngine = webView.getEngine();
+		setPopupHandler(webEngine);
 	}
 
 	@Override
@@ -56,5 +64,25 @@ public class JFXBrowser extends BrowserProxy {
 		public void add(Node newComponent) {
 			getChildren().add(newComponent);
 		}
+	}
+
+	protected static void setPopupHandler(WebEngine we) {
+		we.setCreatePopupHandler(new Callback<PopupFeatures, WebEngine>() {
+			@Override public WebEngine call(PopupFeatures popupFeatures) {
+				final Stage stage = new Stage(StageStyle.UTILITY);
+				final WebView wvWindow = new WebView();
+				stage.setScene(new Scene(wvWindow));
+				stage.show();
+				wvWindow.getEngine().getLoadWorker().stateProperty().addListener(
+						new ChangeListener<Worker.State>() {
+							public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+								if (newState == Worker.State.SUCCEEDED) {
+									stage.setTitle(wvWindow.getEngine().getTitle());
+								}
+							}
+						});
+				setPopupHandler(wvWindow.getEngine());
+				return wvWindow.getEngine();			}
+		});
 	}
 }
